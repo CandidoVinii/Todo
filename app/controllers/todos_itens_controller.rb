41b354d58e6ba9273046d6e6adb40e42d75e4785
CandidoVinii <viniciuscandidoaquino@gmail.com>
2025-04-21 20:28:24 -0300

    feat: adiciona lógica para construir um novo item de tarefa na ação index do controlador TodosItens

--- app/controllers/todos_itens_controller.rb
+++ app/controllers/todos_itens_controller.rb
@@ -3,6 +3,7 @@ class TodosItensController < ApplicationController
   before_action :set_todo, only: [ :edit, :update, :destroy ]
 
   def index
+    @todo = @todo_list.todos_itens.build
   end
 
   def show
@@ -21,5 +22,17 @@ class TodosItensController < ApplicationController
   end
 
   def destroy
+  end
+
+  def set_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def set_todo
+    @todo = @todo_list.todos.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:content, :done)
   end
 end
