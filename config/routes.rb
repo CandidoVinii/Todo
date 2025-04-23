471956fb30adfb0d4c493ffac602988175cea329
CandidoVinii <viniciuscandidoaquino@gmail.com>
2025-04-23 15:58:16 -0300

    fix: corrige a estrutura das rotas para incluir o bloco de recursos de itens de tarefa

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do
       patch :done, on: :member
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
