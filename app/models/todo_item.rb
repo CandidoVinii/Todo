57f1de8eb91c31755591d29daf66375704aba232
CandidoVinii <viniciuscandidoaquino@gmail.com>
2025-04-23 11:24:01 -0300

    fix: corrige formatação da mensagem de validação no modelo TodoItem

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,5 +1,5 @@
 class TodoItem < ApplicationRecord
   belongs_to :todo_list
 
-  validates :content, presence: { message: "Você precisa preencher a descrição da tarefa"}
+  validates :content, presence: { message: "Você precisa preencher a descrição da tarefa" }
 end
