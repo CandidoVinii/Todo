37edfbcd556c5afa9d3a609b916d3a0f9fb0fddb
CandidoVinii <viniciuscandidoaquino@gmail.com>
2025-04-23 22:46:10 -0300

    fix: corrige mensagem de validação para o campo de conteúdo na tarefa

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,5 +1,5 @@
 class TodoItem < ApplicationRecord
   belongs_to :todo_list
 
-  validates :content, presence: { message: "Você precisa preencher a descrição da tarefa" }
+  validates :content, presence: { message: "can't be blank" }
 end
